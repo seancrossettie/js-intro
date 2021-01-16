5d9451980712fe222b8f671f2f7d707b8f726834
Sean <jb@JBs-MacBook-Pro.local>
2021-01-16 13:09:32 -0600

    added logic

--- main.js
+++ main.js
@@ -1,5 +1,19 @@
-console.log("Connected");
+// console.log("Connected");
 
-if (true) {
-  console.log('true')
+  // function isThisTrue(arg1, arg2) {
+  //   if (arg1 !== 'nice' && arg2 !== 'Nice') {
+  //     console.log(`You are ${arg}`);
+  //   } else if (arg2.length < 8) {
+  //     console.log('false')
+  //   }
+  // }
+
+// isThisTrue('nice', 'Nice');
+
+const canDrive = (age) => {
+  if (age >= 15) {
+    return true;
+  }
 }
+
+console.log(canDrive(15));
